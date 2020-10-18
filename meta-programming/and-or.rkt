b4cdc0a2cedd88a2870eaa3779055d6fa82ecc24
Philip Dumaresq <phdumaresq@protonmail.com>
2020-10-18 13:51:03 -0400

    Finished my talk on metaprogramming

--- meta-programming/and-or.rkt
+++ meta-programming/and-or.rkt
@@ -4,5 +4,12 @@
   (syntax-rules ()
     [(_) #true]
     [(_ x) x]
-    [(_ x . xs)
-     (if x x (or xs))]))
+    [(_ x xs ...)
+     (if x x (or xs ...))]))
+
+(define-syntax and
+  (syntax-rules ()
+    [(_) #true]
+    [(_ x) x]
+    [(_ x xs ...)
+     (if x (and xs ...) x)]))
